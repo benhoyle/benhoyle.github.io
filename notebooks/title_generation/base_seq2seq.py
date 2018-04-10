59ab66c23dbca0d85a2926867b99c17e11286902
Ben Hoyle <benjhoyle@gmail.com>
2018-04-10 14:38:28 +0100

    Blog post TG2 + notebook files

---
+++ notebooks/title_generation/base_seq2seq.py
@@ -0,0 +1,379 @@
+import random
+import math
+import os
+
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from keras.preprocessing import text
+from keras.preprocessing.sequence import pad_sequences
+import numpy as np
+
+
+from abstract_model_wrapper import AbstractModelWrapper
+
+
+class BaseSeq2Seq(AbstractModelWrapper):
+    """ Abstract class for sequence to sequence models for mixing in. """
+
+    def __init__(
+        self,
+        encoder_texts=None,
+        decoder_texts=None,
+        encoder_seq_length=None,
+        decoder_seq_length=None,
+        num_encoder_tokens=None,
+        num_decoder_tokens=None,
+        latent_dim=300,
+        weights_file=None,
+        training_set_size=250,  # Due to memory we need to train in sets
+        batch_size=32
+    ):
+        # If encoder / decoder seq_length = none we can set based on data
+        self.encoder_seq_length = encoder_seq_length
+        self.decoder_seq_length = decoder_seq_length
+        # Likewise if the x_tokens are None we can set later based on the data
+        self.num_encoder_tokens = num_encoder_tokens
+        self.num_decoder_tokens = num_decoder_tokens
+        self.latent_dim = latent_dim
+        self.training_set_size = training_set_size
+        self.batch_size = batch_size
+        # If passed with init process texts
+        self.input_tokenizer = None
+        self.output_tokenizer = None
+        self.load_text_data(encoder_texts, decoder_texts)
+        self._start_checks()
+        self.model = None
+        self._build_model()
+        # Build model here?
+        if weights_file and self.model:
+            self.weights_file = weights_file
+
+            self._load_weights()
+        else:
+            self.weights_file = "weights.hdf5"
+        self._reset_metrics()
+
+    # These are the exposed methods (to keep things simple)
+    def load_text_data(self, encoder_texts, decoder_texts):
+        """ Print a representation of the model. """
+        self.encoder_texts = encoder_texts
+        self.decoder_texts = decoder_texts
+
+        # Convert to seqs - this will generate tokenizers
+        self.input_data = self._text2seq(self.encoder_texts, encoder=True)
+        self.output_data = self._text2seq(self.decoder_texts, encoder=False)
+
+        # Set dictionaries - we need dictionaries in each direction
+        self.input_dictionary = self.input_tokenizer.word_index
+        self.output_dictionary = self.output_tokenizer.word_index
+        self.input_dictionary_rev = dict(
+            (i, char) for char, i in self.input_dictionary.items()
+        )
+        self.output_dictionary_rev = dict(
+            (i, char) for char, i in self.output_dictionary.items()
+        )
+
+        o_string = (
+            "Our input data has shape {0} and"
+            " our output data has shape {1}"
+            )
+        print(o_string.format(self.input_data.shape, self.output_data.shape))
+        # Split into test and train
+        self._split_data()
+
+    def predict(self, input_data):
+        """ Predict output text from input text. """
+        # if input is text convert to sequence
+        if isinstance(input_data, str):
+            # If input_text is string then need to convert to single item list
+            input_seq = self._text2seq([input_data], encoder=True)
+        else:
+            input_seq = input_data
+        predicted_output_seq = self._predict_from_seq(input_seq)
+        predicted_text = self._seq2text(predicted_output_seq)
+        return predicted_text
+
+    def train(self, epochs=1, reset_metrics=False):
+        """ Train in batches on all data with validation. """
+        if reset_metrics:
+            self._reset_metrics()
+        for e in range(0, epochs):
+            print("Training for epoch {0}".format(e))
+            self._train_epoch()
+            self._save_weights()
+            self.example_output(5)
+        self.plot_loss()
+
+    def print(self):
+        """ Print model summary."""
+        print("Training Model:\n")
+        print(self.model.summary())
+
+    def _save_weights(self):
+        """ Load weights from file. """
+        self.model.save_weights(self.weights_file, overwrite=True)
+
+    def plot_loss(self):
+        """ Plot training and validation loss. """
+        plt.plot(self.train_loss)
+        plt.plot(self.val_loss)
+        plt.title('Model Loss')
+        plt.ylabel('Loss')
+        plt.xlabel('Iteration')
+        plt.legend(['train', 'test'], loc='upper left')
+        plt.show()
+
+    def example_output(self, number):
+        """ Print a number of example predictions. """
+
+        # Select a set of test data at random
+        num_test_titles = len(self.input_test_data)
+        indices = random.sample(range(0, num_test_titles), number)
+        print("------------------------------------------")
+        for i in indices:
+            input_sample = self.input_test_data[i]
+            output_sample = self.output_test_data[i]
+            predicted_text = self.predict(input_sample)
+            output_sample_text = self._seq2text(output_sample)
+            claim_text = self._seq2text(input_sample, output=False)
+            print("Sample of claim text: {}\n".format(claim_text[0:200]))
+            o_string = (
+                "Predicted title is: {}"
+                " \nActual title is: {} \n---"
+            )
+            print(o_string.format(predicted_text, output_sample_text))
+
+    def _seq2text(self, seq, output=True):
+        """ Convert a sequence of integers to text."""
+        control_ints = [
+            self.output_dictionary["stopseq"],
+            self.output_dictionary["startseq"],
+            0
+        ]
+        text = ''
+        for k in seq:
+            w = ''
+            k = k.astype(int)
+            if output:
+                # Adapted to take account of different control integers
+                if k not in control_ints and k < self.num_decoder_tokens:
+                    w = self.output_dictionary_rev[k]
+            else:
+                # If input
+                if k != 0 and k < self.num_decoder_tokens:
+                    w = self.input_dictionary_rev[k]
+            if w:
+                text = text + w + ' '
+        return text
+
+    def _load_weights(self):
+        """ Load weights from file. """
+        try:
+            # load weights
+            self.model.load_weights(self.weights_file)
+            print("Loaded weights")
+        except:
+            print("No existing weights found")
+
+    def _split_data(self, seed=9, proportion=0.2):
+        # We need to split into train and test data
+        print("Generating training and test data")
+        np.random.seed(seed)
+        # split the data into training (80%) and testing (20%)
+        (
+            self.input_train_data,
+            self.input_test_data,
+            self.output_train_data,
+            self.output_test_data,
+        ) = train_test_split(
+                self.input_data,
+                self.output_data,
+                test_size=proportion,
+                random_state=seed
+            )
+
+    def _get_dataset(self, i, i_end, test=False):
+        """ Get a segment of data from i-i_end. """
+        if test:
+            # Get training dataset
+            dataset = self._generate_dataset(
+                self.input_test_data,
+                self.output_test_data,
+                i, i_end)
+        else:
+            # Get test dataset
+            dataset = self._generate_dataset(
+                self.input_train_data,
+                self.output_train_data,
+                i, i_end)
+        return dataset
+
+    def _train_set(self, training_data, validation_data):
+        """ Perform one training iteration model."""
+        model_inputs, model_outputs = training_data
+        if validation_data:
+            callback = self.model.fit(
+                model_inputs,
+                model_outputs,
+                validation_data=(validation_data),
+                batch_size=self.batch_size,
+                epochs=1
+            )
+        else:
+            callback = self.model.fit(
+                model_inputs,
+                model_outputs,
+                batch_size=self.batch_size,
+                epochs=1
+            )
+        self.train_loss += callback.history['loss']
+        self.val_loss += callback.history['val_loss']
+
+    def _train_epoch(self):
+        """ Training iterations for one epoch - i.e. one pass through the
+        training data. """
+        num_train_examples = len(self.input_train_data)
+        num_test_examples = len(self.input_test_data)
+
+        # Loop here to avoid memory issues with the target one hot vector
+        for i in range(0, num_train_examples, self.training_set_size):
+            if i + self.training_set_size >= num_train_examples:
+                i_end = num_train_examples
+            else:
+                i_end = i + self.training_set_size
+
+            # Generate a range for the test data
+            i_test = math.floor(i * (num_test_examples/num_train_examples))
+            i_test_end = math.floor(
+                i_end * (num_test_examples/num_train_examples)
+                )
+
+            # Generate small sets of train and test data
+            training_data = self._get_dataset(i, i_end, test=False)
+            validation_data = self._get_dataset(i_test, i_test_end, test=True)
+            # Run a training iteration
+            print(
+                "Training on batch {0} to {1} of {2}".format(
+                        i,
+                        i_end,
+                        num_train_examples
+                    )
+                )
+            self._train_set(training_data, validation_data)
+
+    def _reset_metrics(self):
+        """ Reset tracked training metrics. """
+        self.train_loss = []
+        self.val_loss = []
+
+    def _text2seq(self, input_text, encoder=True):
+        """ Convert texts to sequences. """
+        if not (self.input_tokenizer and self.output_tokenizer):
+            # Generate tokenizers
+            self._generate_tokenizers()
+        if encoder:
+            # Convert using encoder configuration
+            tokenizer = self.input_tokenizer
+            padding = 'pre'
+            length = self.encoder_seq_length
+        else:
+            # Convert using decoder configuration
+            tokenizer = self.output_tokenizer
+            padding = 'post'
+            length = self.decoder_seq_length
+        encoded_data = tokenizer.texts_to_sequences(input_text)
+        encoded_data = self._pad_seqs(
+            encoded_data,
+            length,
+            padding
+            )
+        return encoded_data
+
+    def _pad_seqs(self, input_seq_data, length, padding):
+        """ Pad sequences. """
+        return pad_sequences(
+            input_seq_data,
+            maxlen=length,
+            padding=padding
+            )
+
+    # Below are methods that will be customised for particular models
+    def _predict_from_seq(self, seq):
+        """ Predict output sequence from input seq. """
+        predicted_output_seq = ""
+        return predicted_output_seq
+
+    def _start_checks(self):
+        """ Checks to run when initialising. """
+        pass
+
+    def _build_model(self):
+        """ Build the model. """
+        pass
+
+    def _generate_tokenizers(self):
+        """ Generate tokenizers for data. """
+        self.input_tokenizer = ""
+        self.output_tokenizer = ""
+
+    def _generate_dataset(self, X, Y, i, i_end):
+        """ Fill this in for models."""
+        dataset = ""
+        return dataset
+
+
+class SharedGlove(BaseSeq2Seq):
+    """ Extended class to use shared embeddings from Glove file."""
+
+    def _generate_tokenizers(self):
+        """ Generate tokenizers for data. """
+        # This model uses a shared tokenizer for both input/output
+        if self.num_encoder_tokens != self.num_decoder_tokens:
+            # for shared embedding check input/output vocabs are equal
+            raise ValueError
+        print("Fitting tokenizers")
+        self.input_tokenizer = text.Tokenizer(
+                num_words=self.num_encoder_tokens,
+                lower=True,
+                char_level=False,
+                oov_token="<UNK>"
+        )
+        self.input_tokenizer.fit_on_texts(
+            self.encoder_texts + self.decoder_texts
+            )
+        self.output_tokenizer = self.input_tokenizer
+
+    def _load_shared_embedding(self):
+        """ Load Glove embeddings. """
+        print("Loading GloVe 100d embeddings from file")
+        GLOVE_DIR = "glove/"
+
+        embeddings_index = {}
+        # For Python 3 tweaked to add 'rb'
+        f = open(os.path.join(GLOVE_DIR, 'glove.6B.100d.txt'), 'rb')
+        for line in f:
+            values = line.split()
+            # Tweaked to decode the binary text values
+            word = values[0].decode('utf-8')
+            coefs = np.asarray(values[1:], dtype='float32')
+            embeddings_index[word] = coefs
+        f.close()
+        self.word_embedding_size = 100  # As we are using the Glove 100d
+        print('Found {0} word vectors.'.format(len(embeddings_index)))
+        self.embedding_matrix = np.zeros(
+            (self.num_encoder_tokens, self.word_embedding_size)
+            )
+
+        # Filter our vocab to only the used items
+        words = [
+            (w, i) for w, i in self.input_dictionary.items()
+            if int(i) < self.num_encoder_tokens
+            ]
+
+        # This is from https://machinelearningmastery.com/
+        # use-word-embedding-layers-deep-learning-keras/
+        print("Building embedding matrix")
+        for word, i in words:
+            embedding_vector = embeddings_index.get(word)
+            if embedding_vector is not None:
+                self.embedding_matrix[i] = embedding_vector
---
+++ notebooks/title_generation/ludwig_model.py
@@ -0,0 +1,114 @@
+from base_seq2seq import SharedGlove
+import numpy as np
+from keras.models import Model
+from keras.layers import Input, LSTM, Dense, Embedding, concatenate
+import os
+
+class LudwigModel(SharedGlove):
+
+    # I think we can reuse some of this?
+    def _predict_from_seq(self, seq):
+        """ Predict output sequence from input seq. """
+        # reformat input seq
+        input_seq = np.zeros((1, self.encoder_seq_length))
+        input_seq[0, :] = seq
+        flag = 0
+        prob = 1
+        ans_partial = np.zeros((1, self.decoder_seq_length))
+        # Add start token integer to end of ans_partial input - initially [0,0,...BOS]
+        ans_partial[0, -1] = self.output_dictionary["startseq"]  #  the index of the symbol BOS (begin of sentence)
+        # Minus one as last entry is stopseq? But can be full length
+        for k in range(self.decoder_seq_length):
+            # Remember to set infdec as model when building model
+            ye = self.infdec.predict([input_seq, ans_partial])
+            # yel = ye[0,:]
+            # p = np.max(yel) - only really useful as a relative measure of confidence
+            mp = np.argmax(ye)
+            # It is this line that sets how our training data should be arranged - need to change both
+            # the line below shifts the existing ans_partial by 1 to the left - [0, 0, ..., BOS, 0]
+            ans_partial[0, 0:-1] = ans_partial[0, 1:]
+            # This then adds the newly decoded word onto the end of ans_partial
+            ans_partial[0, -1] = mp
+            if mp == self.output_dictionary["stopseq"]:  #  the index of the symbol EOS (end of sentence)
+                break
+            #if flag == 0:
+            #    prob = prob * p
+        predicted_output_seq = ans_partial[0]
+        return predicted_output_seq
+
+    def _generate_dataset(self, X, Y, i, i_end):
+        """ Generate the data for training/validation from X and Y.
+        i_end is the end of the set, i is the start."""
+        set_size = 0
+        limit_list = list()
+        for sent in Y[i:i_end]:
+            # Edited below to use integer value of EOS symbol
+            EOS = self.output_dictionary["stopseq"]
+            limit = np.where(sent==EOS)[0][0]  #  the position of the symbol EOS
+            set_size += limit + 1
+            limit_list.append(limit)
+
+        # Generate blank arrays for the set
+        I_1 = np.zeros((set_size, self.encoder_seq_length))
+        I_2 = np.zeros((set_size, self.decoder_seq_length))
+        # This below is a big array
+        Y_set = np.zeros((set_size, self.num_decoder_tokens))
+        count = 0
+        for l in range(0, (i_end - i)):
+            limit = limit_list[l]
+            # We only need to create examples up to the length of the title
+            for m in range(1, limit+1):
+                # Generate our one-hot y out
+                one_hot_out = np.zeros((1, self.num_decoder_tokens))
+                # This builds our one-hot generation into our training loop
+                # The l and m respectively iterate through the samples and the output sequence elements
+                one_hot_out[0, Y[l+i][m]] = 1
+                # Create a blank row/array for a partial input for our summary model - this is fed into the decoder
+                partial_input = np.zeros((1, self.decoder_seq_length))
+                partial_input[0, -m:] = Y[l+i][0:m]
+                # This fills in each sample of the training data, i.e. count increments up to set size
+                I_1[count, :] = X[l+i]
+                I_2[count, :] = partial_input
+                Y_set[count, :] = one_hot_out
+                count += 1
+
+            # Shuffle the I_1, I_2 and Y_set vectors for better training - trick from RL
+            # - see here - np.take(X,np.random.permutation(X.shape[0]),axis=0,out=X);
+            indices = np.random.permutation(I_1.shape[0])
+            np.take(I_1, indices, axis=0, out=I_1)
+            np.take(I_2, indices, axis=0, out=I_2)
+            np.take(Y_set, indices, axis=0, out=Y_set)
+        return ([I_1, I_2], Y_set)
+
+    def _start_checks(self):
+        """ Checks to run when initialising. """
+        pass
+
+    def _build_model(self):
+        """ Build the model. """
+        print("Building model")
+        self._load_shared_embedding()
+        # source text input model
+        inputs1 = Input(shape=(self.encoder_seq_length,))
+        #am1 = Embedding(X_vocab_len, 128)(inputs1)
+        Shared_Embedding = Embedding(
+            output_dim=self.word_embedding_size,
+            input_dim=self.num_encoder_tokens,
+            weights=[self.embedding_matrix],
+            input_length=self.encoder_seq_length
+        )
+        am1 = Shared_Embedding(inputs1)
+        am2 = LSTM(self.latent_dim)(am1)
+        # summary input model
+        inputs2 = Input(shape=(self.decoder_seq_length,))
+        sm1 = Shared_Embedding(inputs2)
+        sm2 = LSTM(self.latent_dim)(sm1)
+        # decoder output model
+        decoder1 = concatenate([am2, sm2])
+        outputs = Dense(self.num_decoder_tokens, activation='softmax')(decoder1)
+        # tie it together [article, summary] [word]
+        self.model = Model(inputs=[inputs1, inputs2], outputs=outputs)
+        self.infdec = self.model
+        print("Compiling model")
+        self.model.compile(loss='categorical_crossentropy', optimizer='adam')
+
